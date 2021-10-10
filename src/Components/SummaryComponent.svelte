fc789ca4b7f7b58403425e5db3ea53c042e55289
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-10-10 12:30:35 +0200

    Calculate inner volume

--- src/Components/SummaryComponent.svelte
+++ src/Components/SummaryComponent.svelte
@@ -1,17 +1,27 @@
 <script>
+	// import {p} from 'mathjs'
+
 	export let rangeValueHeight = '';
 	export let rangeValueDia = '';
 	export let rangeValueThickness = '';
 
-	let outerDiameter = '';
-	let totalHeight = '';
+	let outerDiameter = 0;
+	let totalHeight = 0;
+	let areaOfFloor = 0;
+	let maxInsideVolume = 0;
 
 	$: {
+		// Convert props to numbers
 		rangeValueHeight = parseInt(rangeValueHeight, 10);
 		rangeValueDia = parseInt(rangeValueDia, 10);
 		rangeValueThickness = parseInt(rangeValueThickness, 10);
+
+		// Calculations
 		outerDiameter = rangeValueDia + rangeValueThickness * 2;
 		totalHeight = rangeValueHeight + rangeValueThickness;
+
+		areaOfFloor = (Math.pow(rangeValueDia / 2, 2) * Math.PI).toFixed(2);
+		maxInsideVolume = (rangeValueHeight * areaOfFloor).toFixed(2);
 	}
 </script>
 
@@ -22,6 +32,18 @@
 		<summary>Outer diameter: {outerDiameter}px</summary>
 		<p>{rangeValueDia} + {rangeValueThickness} * 2</p>
 		<p>Inner diameter + Wall thickness (both sides)</p>
+	</details>
+
+	<details>
+		<summary>Total height: {totalHeight}px</summary>
+		<p>{rangeValueHeight} + {rangeValueThickness} * 2</p>
+		<p>Inner height + Wall thickness</p>
+	</details>
+
+	<details>
+		<summary>Max inner volume: {maxInsideVolume}px^3</summary>
+		<p>{rangeValueHeight} * {areaOfFloor}</p>
+		<p>Inner height + cross-sec-area</p>
 	</details>
 </section>
 
